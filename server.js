766ccd44177cccb59e9d40d55ec083b0957945e7
packleader206 <packleader206@gmail.com>
2019-01-25 09:45:32 -0800

    updated server.js, added burger gif, installed some dependencies

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Eat-Da-Burger - server.js - UW Coding Bootcamp - Week 14 Homework
 
-var express = require("express");
+// Node dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 
 // ==============================================================================
@@ -14,6 +17,26 @@ var app = express();
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
+// Express middleware -- Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + '/public'));
+
+
+// Parse URL
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Router variable requires burger_controller.js
+var router = require('./controllers/burgers_controller.js');
+app.use('/', router);
+
+
+// Start server
+app.listen(PORT, function() 
+{
+  console.log("App listening on PORT " + PORT);
+});
