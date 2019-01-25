f0c83a3aed1c11b9ced1b79a8fb9b4b1d7f6aa78
packleader206 <packleader206@gmail.com>
2019-01-24 18:18:25 -0800

    added file structure

---
+++ server.js
@@ -0,0 +1,19 @@
+// Eat-Da-Burger - server.js - UW Coding Bootcamp - Week 14 Homework
+
+var express = require("express");
+
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
