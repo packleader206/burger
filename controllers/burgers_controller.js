b65c885c6cfdc1bbf42d9c47e857272e1d5dae81
packleader206 <packleader206@gmail.com>
2019-01-25 11:38:58 -0800

    updated the controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,41 @@
+// Eat-Da-Burger - orm.js - UW Coding Bootcamp - Week 14 Homework
+
+// Dependencies
+var express = require('express');
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions
+var burger = require('../models/burger.js');
+
+//Setup Routes
+
+// Index Redirect
+router.get('/', function (req, res) {
+  res.redirect('/index');
+});
+
+// Index Page 
+router.get('/index', function (req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = { burgers: data };
+    //console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
+});
+
+// Create a New Burger
+router.post('/burger/create', function (req, res) {
+  burger.insertOne(req.body.burger_name, function() {
+    res.redirect('/index');
+  });
+});
+
+// Eat Burger
+router.post('/burger/eat/:id', function (req, res) {
+  burger.updateOne(req.params.id, function() {
+    res.redirect('/index');
+  });
+});
+
+// Export routes
+module.exports = router;
