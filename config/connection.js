7792881274a354ff37ac6989492796b32ef23ff2
packleader206 <packleader206@gmail.com>
2019-01-26 12:57:34 -0800

    fixed connection error

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ if (process.env.JAWSDB_URL) {
 } else {
 	// DB is local on localhost
 	connection = mysql.createConnection({
-		port: 8080,
+		port: 3306,
 		host: 'localhost',
 		user: 'root',
 		password: 'aftermath@71!',
