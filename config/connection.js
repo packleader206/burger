ee8d920795541e9f29d7ad49e491f61541635829
packleader206 <packleader206@gmail.com>
2019-01-31 17:49:38 -0800

    updated for readme file

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@
 // Required dependencies
 var mysql = require('mysql');
 
-// require('dotenv').config();
+
 
 // Create the MySQL connection object
 var connection;
@@ -18,7 +18,7 @@ if (process.env.JAWSDB_URL) {
 		port: 3306,
 		host: 'localhost',
 		user: 'root',
-		password: 'aftermath@71!',
+		password: 'your_password',
 		database: 'burgers_db'
 	})
 };
